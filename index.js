4de65e7d0b09f38f194bd83c06f5015149764115
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2023-04-19 17:35:54 -0300

    Etapa 2 completada

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from 'express'
+import articulosFamiliasMockRouter from './routes/articulosFamiliasMock.js'
 
 // Crear el servidor
 const app = express()
+
+// Middleware
+app.use(express.json())
 
 // Controlar la ruta
 app.get('/', (req, res) => {
   res.send('Backend inicial de dds-backend')
 })
+
+// Acceso a la ruta de articulos familia mock
+
+app.use(articulosFamiliasMockRouter)
 
 // Levatar el servidor
 
